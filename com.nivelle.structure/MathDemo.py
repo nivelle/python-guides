1f89f9a59dfdfe1c9df73a84b4685310b857cf42
nivellefu <fuxinzhong@zhangyue.com>
2019-11-09 21:03:26 +0800

    【python】

---
+++ com.nivelle.structure/MathDemo.py
@@ -0,0 +1,26 @@
+def MathFunction1(var1, var2):
+    print('乘法:', var1 * var2)
+
+
+def MathFunction2(var1, var2):
+    print('指数:', var1 ** var2)
+
+
+def MathFunction3(var1, var2):
+    print('除法取余:', var1 % var2)
+
+
+def MathFunction4(var1, var2):
+    print('除法取整:', var1 // var2)
+
+
+def MathFunction5(var1, var2):
+    print('除法:', var1 / var2)
+
+
+if __name__ == "__main__":
+    MathFunction1(3.1231, 2.123)
+    MathFunction2(2, 3)
+    MathFunction3(8, 3)
+    MathFunction4(8, 3)
+    MathFunction5(8, 3)
--- com.nivelle.structure/StrDemo.py
+++ com.nivelle.structure/StrDemo.py
@@ -1,6 +1,11 @@
-def myStr(str1, str2):
-    print("strAppend", str1 + str2)
+def myStr1(str1, str2):
+    print("strAppend:", str1 + str2)
+
+
+def myStr2(str1):
+    print("strCopy:", str1 * 3)
 
 
 if __name__ == "__main__":
-    myStr("hello", "python")
+    myStr1("hello", "python")
+    myStr2("python")
